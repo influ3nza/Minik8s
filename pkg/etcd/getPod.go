e87f0334103dfb57e9dbaa8c72ace9710e8a9501
chenchao <971707558@qq.com>
2024-04-22 11:31:27 +0800

    finish test

--- pkg/etcd/getPod.go
+++ pkg/etcd/getPod.go
@@ -11,35 +11,47 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-func AddPod(filePath string) {
-	var etcdWrap *EtcdWrap = nil
+func AddPod(filePath string, etcdWrap *EtcdWrap) error {
 	file, err := os.Open(filePath)
 	if err != nil {
 		fmt.Printf("connect to etcd failed, err:%v\n", err)
+		return err
 	}
+	print("line 20")
 	content, err := io.ReadAll(file)
 	if err != nil {
 		fmt.Printf("connect to etcd failed, err:%v\n", err)
+		return err
 	}
+	print("line26")
 	pod := &api_obj.Pod{}
+	print()
+	print("line29")
 
 	err = yaml.Unmarshal(content, pod)
 	if err != nil {
 		fmt.Printf("connect to etcd failed, err:%v\n", err)
+		return err
 	}
+	print("line36")
 
 	// 读取的内容转化为json
 	jsonBytes, err := json.Marshal(pod)
 	if err != nil {
 		fmt.Printf("connect to etcd failed, err:%v\n", err)
+		return err
 	}
+	print("line44")
 
 	// 将 *bytes.Reader 转换为 []byte
 	podBytes, err := io.ReadAll(bytes.NewReader(jsonBytes))
 	if err != nil {
 		fmt.Printf("failed to read pod bytes: %v\n", err)
-		return
+		return err
 	}
+	fmt.Println("NodeName:", pod.Spec.NodeName)
+	fmt.Println("PodBytes:", string(jsonBytes))
 
 	etcdWrap.Put(pod.Spec.NodeName, podBytes)
+	return nil
 }
--- pkg/etcd/getPod_test.go
+++ pkg/etcd/getPod_test.go
@@ -2,12 +2,19 @@ package etcd
 
 import (
 	"testing"
-	"my-Minik8s/pkg/etcd"
+	"time"
 )
 
 func TestAddPod(t *testing.T) {
 	filePath := "./testfile/Pod-1.yaml"
-	err := AddPod(filePath)
+	print("test hello")
+	etcdWrap, err := CreateEtcdInstance([]string{"http://localhost:50000"}, 5*time.Second)
+
+	if err != nil {
+		t.Errorf("AddPod returned an error: %v", err)
+	}
+	err = AddPod(filePath, etcdWrap)
+	print("success addpod")
 	if err != nil {
 		t.Errorf("AddPod returned an error: %v", err)
 	}
