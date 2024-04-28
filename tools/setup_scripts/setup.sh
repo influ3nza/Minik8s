52862a7724143fccd4cf71eb95af3563bc8e9cd8
GJX <1393429978@qq.com>
2024-04-28 13:24:56 +0000

    etcd set v3

--- tools/setup_scripts/setup.sh
+++ tools/setup_scripts/setup.sh
@@ -105,8 +105,10 @@ cat << EOF > /etc/cni/net.d/flannel.conflist
 }
 EOF
 
+nerdctl network ls
 echo "flannel.conflist 文件已创建并写入内容"
 
+export ETCDCTL_API=3
 etcdctl --endpoints "http://127.0.0.1:2379" put /coreos.com/network/config '{"NetWork":"10.2.0.0/16","SubnetMin":"10.2.1.0","SubnetMax": "10.2.20.0","Backend": {"Type": "vxlan"}}'
 
 /opt/flannel/flanneld -etcd-endpoints=http://127.0.0.1:2379 &
