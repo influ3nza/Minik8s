1003568c61f5d8222081a65c5621ec6b8ec3dc31
GJX <1393429978@qq.com>
2024-05-04 06:44:39 +0000

    nothing

--- pkg/kubelet/application/kubelet.go
+++ pkg/kubelet/application/kubelet.go
@@ -1,5 +1,9 @@
 package application
 
+import "github.com/gin-gonic/gin"
+
 type Kubelet struct {
 	ApiServerAddress string
+	router           *gin.Engine
+	port             int32
 }
--- pkg/kubelet/application/running.go
+++ pkg/kubelet/application/running.go
@@ -1,16 +1,3 @@
 package application
 
-import "net/http"
-
-type Handler func(response http.ResponseWriter, req *http.Request)
-
-func Run(httpFunc map[string]Handler) {
-	for method, handler := range httpFunc {
-		http.HandleFunc(method, handler)
-	}
-
-	err := http.ListenAndServe(":42100", nil)
-	if err != nil {
-		return
-	}
-}
+func GetPod()
