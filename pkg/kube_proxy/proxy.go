6fcf79d598c7a7c00a6af1bbd3c72dac735ad657
GJX <1393429978@qq.com>
2024-05-08 06:53:28 +0000

    proxy not finish

--- pkg/kube_proxy/proxy.go
+++ pkg/kube_proxy/proxy.go
@@ -1,15 +1,23 @@
 package kube_proxy
 
 import (
-	"github.com/moby/ipvs"
+	"minik8s/pkg/api_obj"
 	"minik8s/pkg/message"
+
+	"github.com/moby/ipvs"
 )
 
 type ProxyManager struct {
 	Consumer    *message.MsgConsumer
 	IpvsHandler *ipvs.Handle
+	Services    *Services
 }
 
 type Manager interface {
-	CreateService()
+	CreateService(srv *api_obj.Service)
+	DeleteService(ip string, port int)
+}
+
+func (m *ProxyManager) CreateService(srv *api_obj.Service) {
+
 }
