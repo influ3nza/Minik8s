22cb9dc6146b8d66e0a2fb6a445e702a03fcc210
GJX <1393429978@qq.com>
2024-05-09 07:47:18 +0000

    kubelet config

--- pkg/config/kubelet/config.go
+++ pkg/config/kubelet/config.go
@@ -3,8 +3,3 @@ package kubelet
 const AddPod string = "/pod/AddPod"
 const DelPod string = "/pod/DelPod/:namespace/:name"
 const GetMatrix string = "/pod/GetMatrix/:namespace/:name"
-
-const Cpu string = "2"
-const Memory string = "8Gi"
-const IpAddress string = "192.168.1.13"
-const Port string = "20000"
