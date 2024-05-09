265d5cbbb7d786c921bc2e58762fa71b42cfe44e
influ3nza <influenza-lv3@sjtu.edu.cn>
2024-05-09 15:40:01 +0800

    moved apiserver config

---
+++ pkg/apiserver/config/etcdConfig.go
@@ -0,0 +1,13 @@
+package config
+
+const (
+	ETCD_node_prefix = "/registry/nodes/"
+
+	//"/registry/pods/:namespace/:name"
+	ETCD_pod_prefix = "/registry/pods/"
+
+	//"/registry/services/:namespace/:name"
+	ETCD_service_prefix = "/registry/services"
+
+	//"/registry/endpoints/:namespace/:podUUID"
+)
