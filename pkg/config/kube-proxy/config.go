5d3c43b7f4646b7d07d7cd5d8276025c78073b1c
GJX <1393429978@qq.com>
2024-05-11 12:57:51 +0000

    config

--- pkg/config/kube-proxy/config.go
+++ pkg/config/kube-proxy/config.go
@@ -1,6 +1,6 @@
 package kube_proxy
 
-const port int32 = 25000
+const Port int32 = 25000
 const CreateService string = "/service/create"
 const DeleteService string = "/service/delete"
 const AddEndpoint string = "/ep/add"
