6d42a1ee752b3bf498f77c436593e391e612973a
chenchao <971707558@qq.com>
2024-05-24 12:18:11 +0800

    unfinish job abstract because of battery off

--- pkg/api_obj/job.go
+++ pkg/api_obj/job.go
@@ -1,4 +1,15 @@
 package api_obj
 
+import "minik8s/pkg/api_obj/obj_inner"
+
 type Job struct {
+	Spec     JobSpec              `yaml:"jobspec" json:"jobspec"`
+	MetaData obj_inner.ObjectMeta `yaml:"metaData" json:"metaData"`
+	Status   JobStatus            `yaml:"status" json:"status"`
+}
+
+type JobSpec struct {
+}
+
+type JobStatus struct {
 }
--- pkg/apiserver/app/jobHandler.go
+++ pkg/apiserver/app/jobHandler.go
@@ -1,1 +1,7 @@
 package app
+
+import "github.com/gin-gonic/gin"
+
+func (s *ApiServer) AddJob(c *gin.Context) {
+
+}
