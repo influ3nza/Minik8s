987207684961fba9807a0309605abdd145e03256
GJX <1393429978@qq.com>
2024-05-24 12:32:19 +0000

    py

--- pkg/serverless/common/server.py
+++ pkg/serverless/common/server.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, Response
+import json
+
+from flask import Flask, request, Response, jsonify
 import func
 
 app = Flask(__name__)
@@ -8,20 +10,24 @@ app = Flask(__name__)
 def handle_request():
     # `params` is a dict
     params = request.json
-    headers = {'Content-Type': 'content/json'}
+    headers = {'Content-Type': 'application/json'}
     try:
         result = func.run(**params)
-        response = Response(str(result), headers=headers, status=200)
+        response = jsonify(result)
+        response.headers = headers
+        response.status = 200
         return response
     except TypeError as e:
-        response = Response("", headers=headers, status=200)
+        response = jsonify({"error": str(e)})
+        response.status_code = 400
+        response.headers = headers
         return response
     except Exception as e:
-        response = Response(str(e), headers=headers, status=500)
-        return str(e)
+        response = jsonify({"error": str(e)})
+        response.status_code = 500
+        response.headers = headers
+        return response
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
-
-
-
