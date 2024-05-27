5b38169efef47bd30dfa64f01b211c77f3235e5f
GJX <1393429978@qq.com>
2024-05-27 08:22:49 +0000

    fix flannel

--- tools/setup_scripts/clear_regenerate_network.sh
+++ tools/setup_scripts/clear_regenerate_network.sh
@@ -8,13 +8,12 @@ if [ -z "$flanneld_pid" ]; then
   echo "flanneld进程未运行"
 else
   # 杀死flanneld进程
-  sudo kill -9 "$flanneld_pid"
+  systemctl stop flannel
   echo "已杀死flanneld进程 (PID: $flanneld_pid)"
 fi
 
 $SCRIPT_DIR/deletcd.sh
 
-export ETCDCTL_API=3
-etcdctl --endpoints "http://127.0.0.1:2379" put /coreos.com/network/config '{"NetWork":"10.2.0.0/16","SubnetMin":"10.2.1.0","SubnetMax": "10.2.20.0","Backend": {"Type": "vxlan"}}'
+etcdctl --endpoints "http://127.0.0.1:2379" put /coreos.com/network/config '{"NetWork":"10.2.0.0/16","SubnetMin":"10.2.1.0","SubnetMax": "10.2.254.0","Backend": {"Type": "vxlan"}}'
 
-/opt/flannel/flanneld -etcd-endpoints=http://127.0.0.1:2379 &
+systemctl restart flannel
